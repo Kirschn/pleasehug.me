7588be2a068c7980791e7e82db775994bd16e272
kirschn <github@kirschn.de>
2017-02-24 11:36:45 +0100

    false

--- redisdumper.js
+++ redisdumper.js
@@ -5,7 +5,7 @@
     var sha256 = require("sha256");
     var sqlConnection = mysql.createConnection(JSON.parse(fs.readFileSync("config.json", "utf8")));
     function dump() {
-      redis.keys("hugs_*", function(err, keys) {
+      redisClient.keys("hugs_*", function(err, keys) {
         console.log(keys)
       })
     }
