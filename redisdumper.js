7014071b3add20a86c19c1766c6185ad84fa5fb3
kirschn <github@kirschn.de>
2017-02-24 11:41:26 +0100

    flds

--- redisdumper.js
+++ redisdumper.js
@@ -6,7 +6,15 @@
     var sqlConnection = mysql.createConnection(JSON.parse(fs.readFileSync("config.json", "utf8")));
     function dump() {
       redisClient.keys("hugs_*", function(err, keys) {
-        console.log(keys)
+        keys.forEach(function(currentKey) {
+          redisClient.get(currentKey, function(err, hugs) {
+            if (err) throw err;
+            var sql = "UPDATE profiles SET hugs = ? WHERE username = ?";
+            sqlConnection.query(sql, [hugs, currentKey.substr(5)], function (err) {
+              if (err) throw err;
+            }
+          })
+        })
       })
     }
     dump();
